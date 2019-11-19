2e5262ea62c95405859c8c62d859e63bb61a4d23
Benoit FONTAINE <benoit.fontaine@outlook.fr>
2019-11-19 20:58:45 +0100

    Ajout des éléments material ui

--- react-app/src/app/App.spec.tsx
+++ react-app/src/app/App.spec.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { mount } from 'enzyme';
+import { CircularProgress } from 'material-ui';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Material UI', () => {
+  test('Doit contenir le MuiThemeProvider', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(MuiThemeProvider).length).toBe(1);
+  });
+
+  test('Doit contenir un cercle de chargement au centre', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('.perfect-middle').length).toBe(1);
+    expect(wrapper.find(CircularProgress).length).toBe(1);
+    expect(wrapper.find(CircularProgress).prop('size')).toBe(180);
+    expect(wrapper.find(CircularProgress).prop('thickness')).toBe(5);
+  });
 });
--- react-app/src/app/App.tsx
+++ react-app/src/app/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.scss';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { CircularProgress } from 'material-ui';
 
-const App: React.FC = () => <div />;
+const App: React.FC = () => (
+  <MuiThemeProvider>
+    <div className="perfect-middle">
+      <CircularProgress size={180} thickness={5} />
+    </div>
+  </MuiThemeProvider>
+);
 
 export default App;
