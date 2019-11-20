633ec31b594d9598ad9d0b557152fd2319d9a1cb
Benoit FONTAINE <benoit.a.fontaine@axa.fr>
2019-11-20 16:34:34 +0100

    ajout du state sur meteo

--- react-app/src/meteo/Meteo.tsx
+++ react-app/src/meteo/Meteo.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Meteo: React.FC = () => <div />;
+const Meteo: React.FC = () => {
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=Lille,france&units=metric&lang=fr&APPID=8842bad529b81b6b7ab08ec713e0b70d'
+    ).then(data => {
+      data.json().then(json => {
+        console.log(json);
+        setDescription(json.weather[0].description);
+      });
+    });
+  });
+
+  return <span id="description">{description}</span>;
+};
 
 export default Meteo;
