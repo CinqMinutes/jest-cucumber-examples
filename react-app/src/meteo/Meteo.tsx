ede28bad9db72db428df42248a5483642acf2fa4
Benoit FONTAINE <benoit.fontaine@outlook.fr>
2019-11-20 23:27:53 +0100

    finish with fetch

--- react-app/src/meteo/Meteo.tsx
+++ react-app/src/meteo/Meteo.tsx
@@ -8,7 +8,6 @@ const Meteo: React.FC = () => {
       'https://api.openweathermap.org/data/2.5/weather?q=Lille,france&units=metric&lang=fr&APPID=8842bad529b81b6b7ab08ec713e0b70d'
     ).then(data => {
       data.json().then(json => {
-        console.log(json);
         setDescription(json.weather[0].description);
       });
     });
--- react-app/src/meteo/meteo.feature.spec.tsx
+++ react-app/src/meteo/meteo.feature.spec.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import AppRouter from '../router';
 import { mount, ReactWrapper } from 'enzyme';
-
 import { MemoryRouter as Router } from 'react-router-dom';
+import { weather } from './__mocks__/sample.weather';
+import { act } from 'react-dom/test-utils';
 
 const feature = loadFeature('./src/meteo/meteo.feature');
 
 defineFeature(feature, test => {
   let wrapper: ReactWrapper;
+  window.fetch = jest
+    .fn()
+    .mockImplementation(() =>
+      Promise.resolve({ json: () => Promise.resolve(weather.result) })
+    );
   test('Aller sur la page permet de saisir le lieu', ({ when, then }) => {
-    when(/^Je vais sur la route '(.*)'$/, url => {
-      wrapper = mount(
-        <Router initialEntries={[url]}>
-          <AppRouter />
-        </Router>
-      );
+    when(/^Je vais sur la route '(.*)'$/, async url => {
+      await act(async () => {
+        wrapper = await mount(
+          <Router initialEntries={[url]}>
+            <AppRouter />
+          </Router>
+        );
+      });
     });
     then(/^J'obtiens la '(.*)' météo '(.*)'$/, (nom, valeur) => {
       expect(wrapper.find(`#${nom}`)).toHaveLength(1);
